1b9896ea4c0073fd77b2fea97f45ba39e449991d
Mvestro <contact@ngarnier.fr>
2021-05-20 21:48:37 +0200

    Initial Commit

---
+++ Shared/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  Shared
+//
+//  Created by Mvestro on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
